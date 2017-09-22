9eabc303ddaeedbf4399da7c8c8a81ef54faadef
Sampler55667892 <y50001028530003@yahoo.co.jp>
2017-09-22 13:21:56 +0900

    VisualTreeExtensions の機能拡張 + 単体テスト追加

--- WpfStackLayer/Extensions/VisualTreeExtensions.cs
+++ WpfStackLayer/Extensions/VisualTreeExtensions.cs
@@ -30,31 +30,57 @@ namespace WpfStackLayer
             }
         }
 
-        // Memo : Name 依存関係プロパティは FrameworkElement で定義されている
+        /// <summary>
+        /// 指定条件を充たした最初の子UIを返します。
+        /// </summary>
+        /// <typeparam name="T">型 (DependencyObject)</typeparam>
+        /// <param name="current">起点</param>
+        /// <param name="predicate">検索条件</param>
+        /// <returns>検索結果</returns>
+        public static T FindFirstChild<T>( this DependencyObject current, Predicate<T> predicate )
+            where T : DependencyObject
+        {
+            int countChildren = VisualTreeHelper.GetChildrenCount( current );
+
+            for (var i = 0; i < countChildren; ++i) {
+                var child = VisualTreeHelper.GetChild( current, i );
+                if (child is T target && predicate( target ))
+                    return target;
+                var found = FindFirstChild( child, predicate );
+                if (found != null)
+                    return found;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// (無条件で) 最初の子UIを返します。
+        /// </summary>
+        /// <typeparam name="T">型 (DependencyObject)</typeparam>
+        /// <param name="current">起点</param>
+        /// <returns>検索結果</returns>
+        public static T FindFirstChild<T>( this DependencyObject current )
+            where T : DependencyObject =>
+            current.FindFirstChild( new Predicate<T>( x => true ) );
+
+        // Memo: Name 依存関係プロパティは FrameworkElement で定義されている
         /// <summary>
         /// 指定した名前に一致した最初の子UIを返します。
         /// </summary>
         /// <param name="current">起点</param>
         /// <param name="targetName">検索対象の名称</param>
         /// <returns>検索結果</returns>
-        public static FrameworkElement FindFirstChild( this FrameworkElement current, string targetName )
-        {
-            int countChildren = VisualTreeHelper.GetChildrenCount( current );
- 
-            for (int i = 0; i < countChildren; ++i) {
-                var child = VisualTreeHelper.GetChild( current, i ) as FrameworkElement;
-                if (child == null)
-                    continue;
- 
-                if (string.Compare( targetName, child.Name ) == 0)
-                    return child;
- 
-                var foundTarget = FindFirstChild( child, targetName );
-                if (foundTarget != null)
-                    return foundTarget;
-            }
- 
-            return null;
-        }
+        public static FrameworkElement FindFirstChild( this FrameworkElement current, string targetName ) =>
+            current.FindFirstChild( new Predicate<FrameworkElement>( x => string.Compare( x.Name, targetName ) == 0 ) );
+
+        /// <summary>
+        /// 自分が指定したUIオブジェクトの (1世代以上上の) 親であるかどうかを判定します。
+        /// </summary>
+        /// <param name="current">起点</param>
+        /// <param name="view">(子であると期待される) UIオブジェクト</param>
+        /// <returns>true: 自分は親、false: それ以外</returns>
+        public static bool IsParentOf( this DependencyObject current, DependencyObject view ) =>
+            current.FindFirstChild( new Predicate<DependencyObject>( x => x == view ) ) != null;
     }
 }
---
+++ WpfStackLayerUnitTest/Extensions/VisualTreeExtensionsTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfStackLayer;
+
+namespace WpfStackLayerUnitTest
+{
+    [TestClass]
+    public class VisualTreeExtensionsTest
+    {
+        [TestMethod]
+        public void FindFirstChild_NoPredicate()
+        {
+            var item1 = new Grid();
+            var item2 = new TextBlock();
+            item1.Children.Add( item2 );
+
+            var found = item1.FindFirstChild<DependencyObject>();
+            found.Is( item2 );
+        }
+
+        [TestMethod]
+        public void FindFirstChild_TargetName_Success()
+        {
+            var name = "test";
+
+            var item1 = new Grid();
+            var item2 = new TextBlock { Name = name };
+            var item3 = new TextBlock();
+
+            item1.Children.Add( item2 );
+            item1.Children.Add( item3 );
+
+            var found = item1.FindFirstChild( name );
+            found.Is( item2 );
+        }
+
+        [TestMethod]
+        public void FindFirstChild_TargetName_Fail()
+        {
+            var name1 = "test1";
+            var name2 = "test2";
+
+            var item1 = new Grid();
+            var item2 = new TextBlock { Name = name1 };
+            var item3 = new TextBlock();
+
+            item1.Children.Add( item2 );
+            item1.Children.Add( item3 );
+
+            var found = item1.FindFirstChild( name2 );
+            found.IsNull();
+        }
+
+        [TestMethod]
+        public void IsParentOf()
+        {
+            var item1 = new Grid();
+            var item2 = new TextBlock();
+            item1.Children.Add( item2 );
+
+            item1.IsParentOf( item2 ).IsTrue();
+            item2.IsParentOf( item1 ).IsFalse();
+        }
+    }
+}
